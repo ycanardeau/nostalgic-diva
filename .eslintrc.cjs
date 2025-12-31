6f3ddc991d97f3bca7a648d0882bae6c73bcebd2
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-12-31 19:58:32 +0900

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,32 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: 'tsconfig.json',
-		sourceType: 'module',
-		tsconfigRootDir: __dirname,
-	},
-	plugins: ['@typescript-eslint/eslint-plugin'],
-	extends: [
-		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended',
-		'react-app',
-	],
 	root: true,
 	env: {
 		node: true,
 		jest: true,
 	},
-	ignorePatterns: ['.eslintrc.cjs'],
-	rules: {
-		'@typescript-eslint/interface-name-prefix': 'off',
-		'@typescript-eslint/explicit-function-return-type': 'error',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-empty-function': 'off',
-		'@typescript-eslint/no-floating-promises': 'error',
-	},
+	ignorePatterns: ['dist'],
+	overrides: [
+		{
+			files: ['**/*.{ts,tsx}'],
+			parser: '@typescript-eslint/parser',
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: __dirname,
+			},
+			plugins: ['@typescript-eslint/eslint-plugin'],
+			extends: [
+				'plugin:@typescript-eslint/recommended',
+				'plugin:prettier/recommended',
+				'react-app',
+			],
+			rules: {
+				'@typescript-eslint/interface-name-prefix': 'off',
+				'@typescript-eslint/explicit-function-return-type': 'error',
+				'@typescript-eslint/explicit-module-boundary-types': 'off',
+				'@typescript-eslint/no-explicit-any': 'off',
+				'@typescript-eslint/no-empty-function': 'off',
+				'@typescript-eslint/no-floating-promises': 'error',
+			},
+		},
+	],
 };
